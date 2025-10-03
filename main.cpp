f677c88cb628961ac17fd1cba9db3d1e5b1ed7b3
Diksha <diksha@macbookpro>
2025-10-02 21:46:01 -0700

    function headers

--- main.cpp
+++ main.cpp
@@ -17,8 +17,27 @@ struct Node {
 // headNode() takes the original head of the Linked List 
 //   and creates a new node and makes it the head of the LL
 // arguments: takes in the head Node of an LL and takes a 
-//   float value
-// returns: <describe returned object>
+//   float value to create a new head node to add to the LL
+void headNode(Node *&head, float val);
+
+// tailNode() takes the head of the Linked List and
+//   creates a new node and makes it the tail of the LL
+// arguments: takes in the head Node of an LL and takes a
+//   float value to create a new node to add to the LL as a tail
+void tailNode(Node *&head, float val);
+
+// delete() takes the head of the Linked List and a position in the LL
+//   where the Node should be deleted
+// arguments: takes in the head Node of an LL and takes an
+//   int value to know what to delete from the LL
+void delete(Node *&head, int place);
+
+// headNode() takes the head of the Linked List and
+//   creates a new node and adds it to the specified place in the LL
+// arguments: takes in the head Node of an LL and takes a
+//   float value to create a new node and place where node is added to LL
+void insert(Node *&head, int place, float val);
+
 
 void output(Node *);
 
